248a8249072ba4a8da137d1b7744ee023fc4a401
Amber Crawford <ammcrawford@gmail.com>
2016-02-29 13:40:49 -0700

    User can follow link to edit link

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -7,6 +7,21 @@ class LinksController < ApplicationController
     @link = Link.new
   end
 
+  def edit
+    @link = Link.find(params["id"])
+  end
+
+  def update
+    @link = Link.find(params[:id])
+    if @link.update(link_params)
+      flash[:notice] = "Successfully Edited Link"
+      redirect_to links_path
+    else
+      flash.now[:error] = "A link must have a name and url"
+      render :edit
+    end
+  end
+
   def create
     @link = current_user.links.new(link_params)
     if @link.save
