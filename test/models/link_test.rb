7a066df264285cf0420ef89807e68500dd62c0fe
Amber Crawford <ammcrawford@gmail.com>
2016-02-29 15:06:56 -0700

    Add model testing for links

--- test/models/link_test.rb
+++ test/models/link_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class LinkTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    user = User.create(email_address: "John", password: "Password")
+    @link = user.links.create(title: "link", url: "https://www.facebook.com/")
+  end
+
+  test "#ideas have a title body and quality" do
+    assert @link.title
+    assert @link.url
+    assert @link.user_id
+  end
+
+  test "#read quality defaults to false" do
+    assert_equal false, @link.read
+  end
 end
